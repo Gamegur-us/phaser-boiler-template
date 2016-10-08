614f83723f3ff2d037cf420057a122fc10ff5cda
eugenioclrc <eugenioclrc@gmail.com>
2016-10-07 21:44:48 -0300

    fullscree + demo

--- src/ts/index.ts
+++ src/ts/index.ts
@@ -1,15 +1,18 @@
 import { Boot, Preload, Stage } from './states/index.ts';
 
+const gameWidth = 480;
+const gameHeight = 720;
+
 export class Game extends Phaser.Game {
   constructor() {
     super({
       enableDebug: false,
       forceSetTimeOut: true,
-      height: 720,
+      height: gameHeight,
       parent: 'game',
       renderer: Phaser.CANVAS,
       transparent: false,
-      width: 480,
+      width: gameWidth,
       // state: Boot,
     });
 
@@ -18,5 +21,53 @@ export class Game extends Phaser.Game {
     this.state.add('Stage', Stage);
 
     this.state.start('Boot');
+
   }
 }
+
+const gameDiv = document.getElementById('game');
+
+window.onload = () => {
+  const game = new Game();
+  const resize = () => {
+    // window.innerHeight
+    // window.innerWidth
+    gameDiv.style.height = window.innerHeight + 'px';
+/*    gameDiv.scrollIntoView(false);
+    gameDiv.requestFullscreen(); // standard
+
+    window.scrollTo(0,1);*/
+    function goFull() {
+      (window as any).screenfull.request(document.body);
+    }
+
+    document.addEventListener('touchstart', goFull, false);
+    document.addEventListener('touchend', goFull, false);
+    document.addEventListener('touchcancel', goFull, false);
+    document.addEventListener('touchmove', goFull, false);
+
+    const currentAspect = window.innerWidth / window.innerHeight;
+    const gameAspectRatio = gameWidth / gameHeight;
+    let w: number;
+    let h: number;
+
+    if (currentAspect > gameAspectRatio) {
+      h = Math.min(window.innerHeight, gameHeight);
+      w = gameAspectRatio * h;
+    } else {
+      w = Math.min(window.innerWidth, gameWidth);
+      h = w / gameAspectRatio;
+    }
+
+    game.width = Math.round(w);
+    game.height = Math.round(h);
+    try {
+      game.scale.refresh();
+    } catch (e) {
+      // empty
+    }
+  };
+
+  window.onresize = resize;
+  resize();
+};
--- src/ts/states/Stage.ts
+++ src/ts/states/Stage.ts
@@ -1,5 +1,17 @@
 export class Stage extends Phaser.State {
+  public preload() {
+    this.load.spritesheet('phaser', 'assets/phaser.png', 70, 90);
+    this.load.image('starfield', 'assets/starfield.jpg');
+  }
+
   public create() {
-    console.log('hello world');
+    this.add.tileSprite(0, 0, 480, 720, 'starfield');
+
+    for (let i = 0; i < 6; i++) {
+      let item = this.add.sprite(this.world.centerX - (69 * 3) + (69 * i), -90, 'phaser', i);
+
+      // Add a simple bounce tween to each character's position.
+      this.add.tween(item).to({y: 240}, 2400, Phaser.Easing.Bounce.Out, true, 1000 + 400 * i);
+    }
   }
 }
