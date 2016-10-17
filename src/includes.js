408ed983cde37b1b5f5f282984e347f430df2ed8
eugenioclrc <eugenioclrc@gmail.com>
2016-10-17 03:35:51 -0300

    path typo

--- src/includes.js
+++ src/includes.js
@@ -3,4 +3,4 @@ require("script!../node_modules/screenfull/dist/screenfull.js")
 // phaser 3
 // require("script!./phaser.js");
 // phaser 2
-require("script!./node_modules/phaser/build/phaser.min.js");
+require("script!../node_modules/phaser/build/phaser.min.js");
